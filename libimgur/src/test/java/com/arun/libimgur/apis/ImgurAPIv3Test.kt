8b993090baf1301cfe9a71f2ff1d4059c7e956ea
Arun Chapagain <chaparun2074@gmail.com>
2024-12-01 22:05:41 +0545

    api test successfull

--- libimgur/src/test/java/com/arun/libimgur/apis/ImgurAPIv3Test.kt
+++ libimgur/src/test/java/com/arun/libimgur/apis/ImgurAPIv3Test.kt
@@ -1,6 +1,5 @@
 package com.arun.libimgur.apis
 
-import com.arun.libimgur.ImgurAPI
 import com.arun.libimgur.services.ImgurAPIv3
 import okhttp3.OkHttpClient
 import org.junit.Assert.assertNotNull
@@ -10,19 +9,19 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 class ImgurAPIv3Test {
 
-  private val client =OkHttpClient.Builder().addInterceptor {
-    val request = it.request().newBuilder()
-      .addHeader("Authorization", "Client-ID b6f98ee072eb23e").build()
-    it.proceed(request)
-  }
-  private val retrofit = Retrofit.Builder()
-    .client(client.build())
-    .addConverterFactory(MoshiConverterFactory.create()).baseUrl("https://api.imgur.com/3/").build()
-  private val api= retrofit.create(ImgurAPIv3::class.java)
-  @Test
+    private val client = OkHttpClient.Builder().addInterceptor {
+        val request = it.request().newBuilder()
+            .addHeader("Authorization", "Client-ID b6f98ee072eb23e").build()
+        it.proceed(request)
+    }
+    private val retrofit = Retrofit.Builder()
+        .client(client.build())
+        .addConverterFactory(MoshiConverterFactory.create()).baseUrl("https://api.imgur.com/3/")
+        .build()
+    private val api = retrofit.create(ImgurAPIv3::class.java)
+    @Test
     fun `get tags working`() {
         val response = api.getTags().execute()
-        println(response.raw())
         assertNotNull(response.body())
-  }
+    }
 }
