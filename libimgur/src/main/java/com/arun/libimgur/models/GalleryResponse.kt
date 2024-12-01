7a114a42e2f358983bc2d163b29b0601d723134d
Arun Chapagain <chaparun2074@gmail.com>
2024-12-01 16:09:16 +0545

    added kotlin interface in Service folder

--- libimgur/src/main/java/com/arun/libimgur/models/GalleryResponse.kt
+++ libimgur/src/main/java/com/arun/libimgur/models/GalleryResponse.kt
@@ -1,6 +1,5 @@
 package com.arun.libimgur.models
 
-
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
--- libimgur/src/main/java/com/arun/libimgur/models/TagsResponse.kt
+++ libimgur/src/main/java/com/arun/libimgur/models/TagsResponse.kt
@@ -1,11 +1,10 @@
 package com.arun.libimgur.models
 
-
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class GalleryTagsResponse(
+data class TagsResponse(
     @Json(name = "data")
     val `data`: Data,
     @Json(name = "status")
@@ -105,8 +104,7 @@ data class GalleryTagsResponse(
                 val views: Int,
                 @Json(name = "vote")
                 val vote: Any?
-            ) {
-            }
+            )
         }
     }
 }
