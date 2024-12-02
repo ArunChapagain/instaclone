067bc707094027c6d2346694f846619b5feaafd2
Arun Chapagain <chaparun2074@gmail.com>
2024-12-02 15:53:03 +0545

    added view binding

--- app/src/main/java/com/arun/instaclone/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/arun/instaclone/ui/feed/FeedFragment.kt
@@ -6,9 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.arun.instaclone.R
-import org.w3c.dom.Text
+
+import com.arun.instaclone.databinding.FragmentFeedBinding
 
 class FeedFragment : Fragment() {
 
@@ -18,22 +17,16 @@ class FeedFragment : Fragment() {
 
     private val viewModel: FeedViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-      val feed=  arguments?.getString("feedType")
-       val rootView= inflater.inflate(R.layout.fragment_feed, container, false)
+      val feed=  arguments?.getString("feedType")//from navigation bar
+        val binding = FragmentFeedBinding.inflate(inflater, container, false)
         feed?.let {
-            rootView.findViewById<TextView>(R.id.tvFeedType).text=it
+            binding.tvFeedType.text= feed
         }
-
-        return rootView
+        return binding.root
     }
 }
